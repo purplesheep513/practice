62681899057d229707b1a739f7bc9de1293d6425
purplesheep513 <prune1231@gmail.com>
2021-09-28 00:38:58 +0900

    여기까지만

--- practice0927.py
+++ practice0927.py
@@ -2,9 +2,32 @@ import sys
 
 target = int(sys.stdin.readline().rstrip())
 numbers = list(map(int,sys.stdin.readline().rstrip().split()))
-visit = [[0,0] for _ in range(len(numbers))]
-dx = [-1,1]
+visit = [[0 for _ in range(len(numbers))] for _ in range(len(numbers))]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+graph = []
+for i in range(len(numbers),0,-1):
+    graph.append([numbers[j] for j in range(i)])
 
-def solution():
+def dfs(x,y):
     answer = 0
-    return answer
+    visit[x][y] = 1
+    stack = []
+    stack.append((x,y,answer))
+    while stack:
+        x,y,answer = stack.pop()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<= nx < len(graph[x]) and 0<=ny < len(graph[y]):
+                if visit[nx][ny] == 0 :
+                    visit[nx][ny] = 1
+                    if dx[i] == 0 :
+                        answer += graph[nx][ny] * dy[i]
+                        stack.append((nx,ny,answer))
+                        print(answer)
+                    if dy[i] == 0:
+                        answer += graph[nx][ny] * dx[i] 
+                        stack.append((nx,ny,answer))
+                        print(answer)
+dfs(0,0)
