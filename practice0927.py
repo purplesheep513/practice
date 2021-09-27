18ee49d52577de3a525990df6a472410f0b1722a
purplesheep513 <prune1231@gmail.com>
2021-09-27 23:59:29 +0900

    일단

--- practice0927.py
+++ practice0927.py
@@ -1,4 +1,10 @@
 import sys
 
-while True:
-    N = int(sys.stdin.readline().rstrip())
+target = int(sys.stdin.readline().rstrip())
+numbers = list(map(int,sys.stdin.readline().rstrip().split()))
+visit = [[0,0] for _ in range(len(numbers))]
+dx = [-1,1]
+
+def solution():
+    answer = 0
+    return answer
