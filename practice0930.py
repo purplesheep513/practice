331da4df23ed71856a80293c197396424d94bf7e
purplesheep513 <prune1231@gmail.com>
2021-10-01 22:21:06 +0900

    더 고민해보자

--- practice0930.py
+++ practice0930.py
@@ -1,22 +1,10 @@
 import sys
+from collections import deque
 
 def solution(begin, target, words):
     answer = 0
-    cnt = 0
-    if target not in words:
-        return 0
-    for i in words:
-        if check_word(cnt, i, begin) == 1:
-            return
+
     return answer
-
-def check_word (cnt, word, begin):
-    for j in list(word).sort():
-        for k in list(begin).sort():
-            if j == k :
-                cnt += 1
-    return cnt
-
 
 begin = "hit"
 target = "cog"
