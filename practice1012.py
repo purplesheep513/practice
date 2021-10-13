9bff2410c5316cdf04e82d95c31fb9d41c0fca53
purplesheep513 <prune1231@gmail.com>
2021-10-13 23:36:55 +0900

    풀었다

--- practice1012.py
+++ practice1012.py
@@ -1,17 +1,35 @@
 def solution(s):
     splited_string = ''
+    answer = len(s)
     for i in range(1,len(s)):
         splited_string = split_string(s,i)
+        result = ''
+        cnt = 1
         for i in range(0,len(s),len(splited_string)):
-            print(s[i:i+len(splited_string)])
-            
-    answer = 0
+            if i <= len(s)-2 * len(splited_string):
+                if s[i:i+len(splited_string)] == s[i+len(splited_string):i + 2 * len(splited_string)]:
+                    cnt += 1
+                else : 
+                    if cnt != 1:
+                        result += str(cnt) + s[i:i+len(splited_string)]
+                        cnt = 1
+                    else : 
+                        result += s[i:i+len(splited_string)]
+            else : 
+                if len(s) - i >= len(splited_string) :
+                    if cnt != 1:
+                        result += str(cnt) + s[i:len(s)]
+                        cnt = 1
+                    else : 
+                        result += s[i:len(s)]
+        answer = min(len(result), answer)
+        print(result)
     return answer
 
 def split_string(s,i):
     s = s[0:i]
     return s
 
-s = "aabbaccc"
+s = "a"
 
-solution(s)
+print(solution(s))
