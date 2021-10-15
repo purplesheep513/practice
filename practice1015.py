c4038332f9e6c3cbeed529ac7f04860f51ddff7c
purplesheep513 <prune1231@gmail.com>
2021-10-15 21:52:32 +0900

    내일 시험이니 좀 쉬운것

--- practice1015.py
+++ practice1015.py
@@ -1,25 +1,13 @@
 import sys
-from collections import deque
 
 N = int(sys.stdin.readline().rstrip())
-X,Y = map(int, sys.stdin.readline().rstrip().split())
-T = int(sys.stdin.readline().rstrip())
-visit = [0 for _ in range(N + 1)]
-graph = [[] for _ in range(N + 1)]
-for _ in range(T):
-    n,m = map(int, sys.stdin.readline().rstrip().split())
-    graph[n].append(m)
-    graph[m].append(n)
+def solution(answer):
+    if answer < 1:
+        return 1
+    if answer > 1:
+        answer = answer * solution(answer-1)
+    return answer
 
-def bfs(x,y):
-    queue = deque([x])
-    visit[x] = 1
-    while queue : 
-        x = queue.popleft()
-        for i in graph[x]:
-            if visit[i] == 0:
-                visit[i] += visit[x] +1
-                queue.append(i)
-    return visit[y]
+ans = solution(N)
 
-print(bfs(X,Y)-1)
+print(ans)
