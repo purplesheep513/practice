dfcda263d89524c5944f5e6dd2d75ed3cc0041b1
purplesheep513 <prune1231@gmail.com>
2021-10-23 10:13:42 +0900

    다음에 푼다

--- practice1022.py
+++ practice1022.py
@@ -1,5 +1,15 @@
-# 솔루션이 잘 생각나지 않았다...ㅠㅠ
+from datetime import datetime
+
+dateFormatter = "%Y, %B %d, %A, %H:%M:%S"
 
 def solution(lines):
+    print(datetime.strptime(lines[0],dateFormatter) - lines[1].replace("s",""))
     answer = 0
     return answer
+
+lines = [
+"2016-09-15 01:00:04.001 2.0s",
+"2016-09-15 01:00:07.000 2s"
+]
+
+solution(lines)
