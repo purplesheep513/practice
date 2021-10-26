b600a6d281356a3b379ed06763ab46102bf9051d
purplesheep513 <prune1231@gmail.com>
2021-10-26 23:48:57 +0900

    오픈채팅방

--- practice1026.py
+++ practice1026.py
@@ -17,3 +17,7 @@ def solution(record):
             answer.append(dict[b]+"님이 나갔습니다.")
 
     return answer
+
+record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+
+solution(record)
