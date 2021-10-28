44b0a2d56fcbba86d665b9214d9c2e67c05e08a8
purplesheep513 <prune1231@gmail.com>
2021-10-28 22:07:53 +0900

    메뉴 리뉴얼 풀기 실패 ..ㅠㅠ

---
+++ practice1028.py
@@ -0,0 +1,13 @@
+def solution(orders, course):
+    answer = []
+    temp = orders[0]
+
+    return answer
+
+
+orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
+course = [2,3,5]
+
+print(solution(orders, course))
+
+# 못 풀었다.... ㅠㅠ
